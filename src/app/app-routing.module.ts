e09b3dc0443010e3558963a7028eb15e77be313a
Ivo Georgiev <ivg_ho@abv.bg>
2023-01-03 20:27:11 +0200

    starting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditAddressComponent } from './edit-address/edit-address.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ProductsingleComponent } from './productsingle/productsingle.component';
+import { ProfileDetailsComponent } from './profile-details/profile-details.component';
+import { ShopComponent } from './shop/shop.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  {path: 'signup', component: SignupComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'checkout', component: CheckoutComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'address', component: EditAddressComponent},
+  {path: 'forgot-password', component: ForgotPasswordComponent},
+  {path: 'order', component: OrdersComponent},
+  {path: 'product', component: ProductsingleComponent},
+  {path: 'profile', component: ProfileDetailsComponent},
+  {path: 'shop', component: ShopComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
